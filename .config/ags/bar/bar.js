49ff65746aed1f028d958de84cf817a55a17881c
n3rdium <n3rdium@gmail.com>
2024-04-06 21:41:10 +0530

    Some more modifications

--- .config/ags/bar/bar.js
+++ .config/ags/bar/bar.js
@@ -1,12 +1,15 @@
 const Bar = () => Widget.Window({
-    name: `StatusBar`,
+    name: `Bar`,
     anchor: ['top', 'left', 'right'],
-    margins: [4, 4],
-    css: 'padding: 1px; background: transparent; border: solid 0px white;',
+    margins: [0, 8],
+    css: 'background: transparent;',
     exclusivity: 'exclusive',
     child: Widget.Box({
-        css: 'padding: 4px;',
-        child: Widget.Label('Hi there!')
+        css: 'padding: 4px; min-height: 24px; border-radius: 6px; background-color: rgba(255, 255, 255, 1); color: white; margin-top: 8px;',
+        child: Widget.Label({
+            label: 'I am a bar',
+            css: 'color: black;'
+        })
     }),
 })
 
--- .config/ags/config.js
+++ .config/ags/config.js
@@ -1,9 +1,13 @@
-import Bar from "./bar/bar.js"
+import Bar from "./bar/bar.js";
+import RightPane from "./rightpane/rightpane.js";
 
 App.config({
     style: './style.css',
 })
 
 export default {
-    windows: [ Bar() ],
+    windows: [
+        Bar(),
+        // RightPane() // TODO: Revealer for the right pane
+    ],
 }
