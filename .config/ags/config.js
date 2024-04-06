e9af4ccd7b5bf3678707fb6581e357ca3594b5d7
n3rdium <n3rdium@gmail.com>
2024-04-06 20:12:32 +0530

    Remaking all `ags` stuff

--- .config/ags/config.js
+++ .config/ags/config.js
@@ -1,23 +1,3 @@
-const hyprland = await Service.import('hyprland');
-
-import { changeWallpaper } from "./Wallpaper/wallpaper_changer.js";
-import Dock from "./Dock/dock.js";
-import Media from "./Media/media.js";
-import StatusBar from "./StatusBar/statusbar.js";
-import delay from "./delay.js";
-
-hyprland.active.workspace.connect('changed', async () => {
-    await delay(64)
-    changeWallpaper(hyprland.active.workspace.id)
-})
-
-Utils.execAsync('/bin/python /home/n3rdium/.config/ags/superkey.py')
-
-// TODO: Live css udpates for each window with utils.monitor
 export default {
-    windows: [
-        Dock(),
-        Media(),
-        StatusBar()
-    ],
+    windows: [],
 }
