5b7f9ae203c9b1942c02c7817797283851c9acf2
n3rdium <n3rdium@gmail.com>
2024-05-21 21:20:48 +0530

    Remake all AGS stuff (again)

--- .config/ags/config.js
+++ .config/ags/config.js
@@ -1,15 +1,8 @@
-import Dock from "./dock/dock.js";
-
 App.config({
     style: './style.css',
 })
 
-// Start the key listener
-Utils.execAsync('python /home/n3rdium/.config/ags/listener.py')
-
 // TODO make the top bar extra thin
 export default {
-    windows: [
-        Dock()
-    ],
+    windows: [],
 }
