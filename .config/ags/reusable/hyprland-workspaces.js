cf865d7db87833840c9585b3e0da7c4b20debf34
n3rdium <n3rdium@gmail.com>
2024-05-24 10:52:01 +0530

    Getting somewhere with AGS

--- .config/ags/reusable/hyprland-workspaces.js
+++ .config/ags/reusable/hyprland-workspaces.js
@@ -1,20 +1,19 @@
 const hyprland = await Service.import('hyprland');
 
 const Workspaces = (
-    icon_css,
+    css,
     workspaces,
     vertical = false,
-    css = 'background: black; min-height: 10px; min-width: 10px;'
 ) => Widget.Box({
     vertical: vertical,
     homogeneous: true,
-    css: css,
+    css: css.box,
     children: hyprland.active.workspace.bind('id').as(x => {
         return workspaces.map(i => {
             return Widget.Button({
                 child: Widget.Label({
                     label: i.icon,
-                    css: i.id != x ? icon_css.inactive : icon_css.active,
+                    css: i.id != x ? css.inactive : css.active,
                 }),
                 on_clicked: () => {
                     hyprland.messageAsync(`dispatch workspace ${i.id}`);
--- .config/ags/topbar/topbar.js
+++ .config/ags/topbar/topbar.js
@@ -1,14 +1,16 @@
 // import WindowTitle from "../reusable/hyprland-window-title.js";
 import Workspaces from "../reusable/hyprland-workspaces.js";
+import SystemTray from "../reusable/system-tray.js";
 
 // CSS stuff for the workspaces widget thing
-const ACTIVE_CSS   = "font-size: 20px; color: white; background: black; min-width: 32px; min-height: 32px; font-family: FiraCode; padding-right: 3px;";
-const INACTIVE_CSS = "font-size: 20px; color: black; background: white; min-width: 32px; min-height: 32px; font-family: FiraCode; padding-right: 3px;";
+const ACTIVE_CSS   = "font-size: 20px; color: white; background: transparent; min-width: 32px; min-height: 32px; font-family: FiraCode; padding-right: 3px;";
+const INACTIVE_CSS = "font-size: 20px; color: gray; background: transparent; min-width: 32px; min-height: 32px; font-family: FiraCode; padding-right: 3px;";
 
 // Workspaces
-const WorkspacesWidget = Workspaces({
+const WorkspacesWidget = () => Workspaces({
         active: ACTIVE_CSS,
         inactive: INACTIVE_CSS,
+        box: 'background-color: black; min-width: 10px; min-height: 38px; margin-top: 4px; margin-left: 4px; padding-left: 4px; padding-right: 4px; border-radius: 6px;'
     },
     [
         {
@@ -58,6 +60,49 @@ const WorkspacesWidget = Workspaces({
     ]
 )
 
+// Aligned layouts
+const LeftLayout = () => Widget.Box({
+    vertical: false,
+    homogeneous: false,
+    spacing: 0,
+    children: [
+        WorkspacesWidget(),
+        Widget.Box({ hexpand: true })
+    ]
+})
+
+const CenterLayout = () => Widget.Box({
+    vertical: false,
+    homogeneous: false,
+    spacing: 0,
+    children: [
+        Widget.Box({ hexpand: true })
+    ]
+})
+
+const RightLayout = () => Widget.Box({
+    vertical: false,
+    homogeneous: false,
+    spacing: 0,
+    children: [
+        Widget.Box({ hexpand: true }),
+        SystemTray(
+            'background-color: black; min-width: 10px; min-height: 38px; margin-top: 4px; margin-right: 4px; padding-left: 8px; padding-right: 8px; border-radius: 6px;',
+            'font-size: 20px;'
+        )
+    ]
+})
+
+// The main box layout thingy for the whole top bar
+const Layout = () => Widget.CenterBox({
+    vertical: false,
+    homogeneous: false,
+    spacing: 0,
+    start_widget: LeftLayout(),
+    center_widget: CenterLayout(),
+    end_widget: RightLayout()
+})
+
 const TopBar = () => Widget.Window({
     name: `TopBar`,
     class_name: "bar",
@@ -65,7 +110,7 @@ const TopBar = () => Widget.Window({
     exclusivity: "exclusive",
     class_names: ['barwin'],
     css: "min-height: 128px; min-width: 128px;",
-    child: WorkspacesWidget
+    child: Layout()
 })
 
 export default TopBar;
