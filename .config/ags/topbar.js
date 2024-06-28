db765cc706da74f084da6a6df0231cb2bf36eef4
n3rdium <n3rdium@gmail.com>
2024-06-28 18:58:54 +0530

    Update the top bar

--- .config/ags/topbar.js
+++ .config/ags/topbar.js
@@ -6,14 +6,14 @@ import PowerProfiles from "./reusable/power-profiles.js";
 import QuickActions from "./reusable/quick-actions.js";
 
 // CSS stuff for the workspaces widget thing
-const ACTIVE_CSS   = "font-size: 20px; color: white; background: transparent; min-width: 32px; min-height: 32px; font-family: FiraCode; padding-right: 3px;";
-const INACTIVE_CSS = "font-size: 20px; color: gray; background: transparent; min-width: 32px; min-height: 32px; font-family: FiraCode; padding-right: 3px;";
+const ACTIVE_CSS   = "font-size: 24px; color: white; background: transparent; min-width: 24px; min-height: 24px; font-family: FiraCode; padding-right: 8px;";
+const INACTIVE_CSS = "font-size: 20px; color: gray; background: transparent; min-width: 24px; min-height: 24px; font-family: FiraCode; padding-right: 8px;";
 
 // Workspaces
 const WorkspacesWidget = () => Workspaces({
         active: ACTIVE_CSS,
         inactive: INACTIVE_CSS,
-        box: 'background: #2e3440; min-width: 10px; min-height: 38px; margin-top: 6px; margin-left: 6px; padding-left: 4px; padding-right: 4px; border-radius: 8px;'
+        box: 'background: #2e3440; min-width: 10px; min-height: 40px; margin-top: 8px; margin-left: 6px; margin-bottom: 2px; padding-left: 6px; padding-right: 4px; border-radius: 6px;'
     },
     [
         {
@@ -80,11 +80,11 @@ const CenterLayout = () => Widget.Box({
     spacing: 0,
     children: [
         Widget.Box({
-            css: 'background: #2e3440; min-width: 10px; min-height: 38px; margin-top: 6px; margin-right: 6px; padding-left: 10px; padding-right: 10px; border-radius: 8px;',
+			css: 'background: #2e3440; min-width: 10px; min-height: 40px; margin-top: 8px; margin-bottom: 2px; margin-right: 8px; padding-left: 10px; padding-right: 10px; border-radius: 8px;',
             child: ActiveIcon('font-family: FiraCode; font-size: 20px;', { 'Code': 'Visual Studio Code' }, 'application-x-executable')
         }),
         Widget.Box({
-            css: 'background: #2e3440; min-width: 10px; min-height: 38px; margin-top: 6px; padding-left: 12px; padding-right: 12px; border-radius: 8px;',
+			css: 'background: #2e3440; min-width: 10px; min-height: 40px; margin-top: 8px; margin-bottom: 2px; padding-left: 12px; padding-right: 12px; border-radius: 8px;',
             child: WindowTitle('font-family: FiraCode; font-size: 12px;', '[ N 0 T H 1 N G ]')
         })
     ]
@@ -97,7 +97,7 @@ const RightLayout = () => Widget.Box({
     children: [
         Widget.Box({ hexpand: true }),
         Widget.Box({
-            css: 'background: #2e3440; min-width: 10px; min-height: 38px; margin-top: 6px; margin-right: 6px; padding-left: 12px; padding-right: 12px; border-radius: 8px;',
+			css: 'background: #2e3440; min-width: 10px; min-height: 40px; margin-top: 8px; margin-bottom: 2px; margin-right: 8px; padding-left: 12px; padding-right: 12px; border-radius: 8px;',
             child: PowerProfiles(
                 { 'performance': '󰡴', 'balanced': '󰊚', 'power-saver': '󰡳' },
                 'font-family: FiraCode; font-size: 20px; margin-right: 8px;',
@@ -105,7 +105,7 @@ const RightLayout = () => Widget.Box({
             )
         }),
         SystemTray(
-            'background: #2e3440; min-width: 10px; min-height: 38px; margin-top: 6px; margin-right: 6px; padding-left: 4px; padding-right: 4px; border-radius: 8px;',
+            'background: #2e3440; min-width: 10px; min-height: 38px; margin-top: 8px; margin-bottom: 2px; margin-right: 8px; padding-left: 4px; padding-right: 4px; border-radius: 8px;',
             'font-size: 20px; min-width: 32px; min-height: 32px;'
         ),
         QuickActions(
@@ -113,7 +113,7 @@ const RightLayout = () => Widget.Box({
                 { icon: '󰂚', tooltip: 'Notification Centre', action: () => { console.log('TODO: Notification Centre') } },
                 { icon: '', tooltip: 'Quick Settings', action: () => { console.log('TODO: Quick Settings') } }
             ], 
-            'background: #2e3440; min-width: 10px; min-height: 38px; margin-top: 6px; margin-right: 6px; padding-left: 10px; padding-right: 4px; border-radius: 8px;',
+            'background: #2e3440; min-width: 10px; min-height: 38px; margin-top: 8px; margin-bottom: 2px; margin-right: 6px; padding-left: 10px; padding-right: 4px; border-radius: 8px;',
             'font-size: 20px; margin-right: 14px;',
         )
     ]
